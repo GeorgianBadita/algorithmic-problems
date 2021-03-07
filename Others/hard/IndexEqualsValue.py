3da6defeff5fa0170b22b3a1aac0596890b19151
GeorgianBadita <geo.badita@gmail.com>
2021-03-07 18:03:57 +0000

    Solve indentation error

--- Others/hard/IndexEqualsValue.py
+++ Others/hard/IndexEqualsValue.py
@@ -4,13 +4,13 @@ def indexEqualsValue(array):
     while left <= right:
         mid = left + (right - left) // 2
 
-	    if array[mid] < mid:
-			left = mid + 1
-		elif array[mid] == mid and mid == 0:
-			return mid
-		elif array[mid] == mid and array[mid - 1] < mid - 1:
-			return mid
-		else:
-			right = mid - 1
+        if array[mid] < mid:
+            left = mid + 1
+        elif array[mid] == mid and mid == 0:
+            return mid
+        elif array[mid] == mid and array[mid - 1] < mid - 1:
+            return mid
+        else:
+            right = mid - 1
 
-	return -1
+    return -1
